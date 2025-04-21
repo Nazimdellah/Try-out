55c07e85c30b944366d29352c717ebf9891ccf71
Nazim <2378684@cegepmontpetit.ca>
2025-04-20 22:28:40 -0400

    e

--- Final_Formatif_A24/ngAnimations/src/app/app.component.ts
+++ Final_Formatif_A24/ngAnimations/src/app/app.component.ts
@@ -1,15 +1,25 @@
 import { transition, trigger, useAnimation } from '@angular/animations';
 import { Component } from '@angular/core';
-import { bounce, shakeX, tada } from 'ng-animate';
+import { bounce, shake, shakeX, tada } from 'ng-animate';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('shake', [transition('* => *', useAnimation(shakeX, { params: { timing: 2, delay: 0} }))]),
+    trigger('bounce', [transition('* => *', useAnimation(bounce, { params: { timing: 2, delay: 0 } }))]),
+  ],
+
 })
 export class AppComponent {
   title = 'ngAnimations';
+  shakeState = false;
+
 
   constructor() {
+  }
+  triggerShake() {
+    this.shakeState = !this.shakeState; // Alterne l'état pour relancer l'animation
   }
 }
